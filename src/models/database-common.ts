adeb5d9657655e0d4549c3abf4b1dd1e0f7962d3
phuongngoxuan <nxphuongktvt@gmail.com>
2023-03-16 16:30:38 +0700

    feat: develop

--- src/models/database-common.ts
+++ src/models/database-common.ts
@@ -3,11 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from 'src/models/repositories/users.repository';
 import { CrawlStatusRepository } from './repositories/crawler.repository';
 import { HistoriesRepository } from './repositories/histories.repository';
+import { MessageChatRepository } from './repositories/message-chats.repositoty';
+import { PetsRepository } from './repositories/pets.repository';
+import { ProceedPetExchangesRepository } from './repositories/proceed-pet-exchanges.repository';
 
 const commonRepositories = [
   UserRepository,
   CrawlStatusRepository,
   HistoriesRepository,
+  MessageChatRepository,
+  PetsRepository,
+  ProceedPetExchangesRepository,
 ];
 
 @Global()
--- src/models/repositories/message-chats.repositoty.ts
+++ src/models/repositories/message-chats.repositoty.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { MessageChatEntity } from '../entities/message-chats.entity';
- 
+
 @EntityRepository(MessageChatEntity)
 export class MessageChatRepository extends Repository<MessageChatEntity> {}
